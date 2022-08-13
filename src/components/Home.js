9876cf96af33b0ed5d7b2bb664464b48568ead98
Daniel Obare <danielobare551@gmail.com>
2022-08-13 14:46:00 +0300

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
+
 import React from "react";
+import {username, city} from "../data/user";
 
 function Home() {
   return (
+    
     <div id="home">
       <h1>
         {username} is a Web Developer from {city}
@@ -9,3 +12,5 @@ function Home() {
     </div>
   );
 }
+
+export default Home
